146320a74e0c75fcb50c641656506d4852ef54ed
Sven_Gabrielle109 <sheepclothedwolf@gmail.com>
2025-09-25 13:02:53 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+from pyscript import display
+
+
+family = "Rachelle L. Damondamon (Mother) , Ronie V. Damondamon (Father), Gabrielle D. Damondamon (Me)"
+personal_life = "Playing online games, watching vidoes, reading e-books and books, listening to music, and hanging out with friends"
+education = "OB. Montessori Center, Fairview Branch"
+hobbies = "Coding", "Gaming", "Art and Design", "Reading"
+friends = "Trisha Paz", "Cristina Tues", "Denise Juachan"
+cultural_background = "Filipino", "Christian"
+skills= "Computer Technology, Painting and Drawing"
+
+display(family, target="family",)
+display(personal_life, target="personal_life")
+display(education, target="education")
+display(hobbies, target="hobbies")
+display(friends, target="friends")
+display(cultural_background, target="cultural_background")
+display(skills, target="skills")
+
+
+fact1 = "I have a pet dog named Lauwin."
+fact2 = "I love to draw and paint in my free time."
+fact3 = "I enjoy playing online games with my friends."
+fact4 = "My favorite cuisine is Italian, especially pasta dishes, and Filipino food."
+fact5 = "I usually read books and e-books about fantasy and adventure."
+fact6 = "I can speak both English and Filipino. BUT, I prefer speaking in English"
+fact7 = "I also love listening to music, especially pop and rock genres."
+fact8 = "I studied in OB Montessori Center, Fairview Branch since I was in Kindergarden until Junior High."
+
+
+display(fact1, target="fact1")
+display(fact2, target="fact2")
+display(fact3, target="fact3")
+display(fact4, target="fact4")
+display(fact5, target="fact5")
+display(fact6, target="fact6")
+display(fact7, target="fact7")
+display(fact8, target="fact8")
